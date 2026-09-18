c5e57a6d670c572e555fe2fe2df8d27dd85f72e1
Bianca Gabriely <bianca.gabriely@escolar.ifrn.edu.br>
2026-09-18 14:43:18 -0300

    Adiciona projeto CleanText

---
+++ script.js
@@ -0,0 +1,13 @@
+const numero = document.getElementById("numero");
+const resultado = document.getElementById("resultado")
+
+function apagar(){
+
+    if (numero.value.trim() === "") {
+       alert("Informe uma sequência de números.")
+        return;
+    }
+   const numeroFinal = numero.value.replace(/\s/g,"");
+
+   resultado.textContent = `Número sem espaços: ${numeroFinal}` ;
+}
